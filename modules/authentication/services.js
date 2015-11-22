78ef008faaaecf2134dc9100ee0d598b10c7057f
Anand Hegde <anandhegde@Anands-MacBook-Pro.local>
2015-11-22 10:34:19 +0530

    Simple login page with hardcoded credentials. No server side

---
+++ modules/authentication/services.js
@@ -0,0 +1,73 @@
+﻿'use strict';
+
+var authMod = angular.module('Authentication')
+
+authMod.factory('AuthenticationService',
+    ['$http', '$cookieStore', '$rootScope', '$timeout',
+    function ($http, $cookieStore, $rootScope, $timeout) {
+        var service = {};
+
+        service.Login = function (username, password, callback) {
+
+            /* Dummy authentication for testing, uses $timeout to simulate api call
+             ----------------------------------------------*/
+            $timeout(function () {
+
+                var response = {};
+                if(username === 'test' && password === 'test'){
+                    response.success = true;
+                    
+                }
+
+                if (!response.success) {
+                    response.message = 'Username or password is incorrect';
+                }
+                callback(response);
+            }, 1000);
+
+
+            /* Use this for real authentication
+             ----------------------------------------------*/
+            //$http.post('/api/authenticate', { username: username, password: password })
+            //    .success(function (response) {
+            //        callback(response);
+            //    });
+
+        };
+
+        service.SetCredentials = function (username, password) {
+            var authdata = btoa(username + ':' + password);
+
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    authdata: authdata
+                }
+            };
+
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        };
+
+        service.ClearCredentials = function () {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic ';
+        };
+
+        return service;
+    }]);
+
+
+
+authMod.factory('UserService', function() {
+     
+    var service = {}; 
+ 
+    service.getUserDetails = function() {
+        return { firstName: 'Anand', lastName: 'Hegde', age: 24, place: 'Manipal'} ;
+    }
+ 
+   
+    return service;
+});
---
+++ modules/home/controllers.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+angular.module('Home')
+
+.controller('HomeController',
+    ['$scope', '$rootScope', 'UserService',
+    function ($scope, $rootScope, UserService) {
+
+    	$scope.loggedInUser = UserService.getUserDetails();
+
+    }]);
